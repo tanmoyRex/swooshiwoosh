223ef42f95cdb0e226e9528f0c13f978d8cc9343
tanmoyRex <tanmoyRex@users.noreply.github.com>
2017-12-10 20:02:47 +0600

    lesson02

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height) // put swoosh logo in the middle of the whole frame
+        
+        bgImg.frame = view.frame; // back ground takes up all the screen space
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
