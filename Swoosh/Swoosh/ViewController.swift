d4eaaccac6ef30e394f7ab5fbf750fb72da51723
tanmoyRex <tanmoyRex@users.noreply.github.com>
2017-12-10 22:19:19 +0600

    lesson03 constraints

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -15,10 +15,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height) // put swoosh logo in the middle of the whole frame
         
-        bgImg.frame = view.frame; // back ground takes up all the screen space
-        
+//        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height) // put swoosh logo in the middle of the whole frame
+//
+//        bgImg.frame = view.frame; // back ground takes up all the screen space
         
         
         // Do any additional setup after loading the view, typically from a nib.
