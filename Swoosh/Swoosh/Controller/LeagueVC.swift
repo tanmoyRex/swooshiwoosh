b5a72a3c56b7b7fb07eed2cc9df6848bad2f2338
tanmoyRex <tanmoyRex@users.noreply.github.com>
2017-12-13 22:21:15 +0600

    lesson08 done - created a Model Player

--- Swoosh/Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -9,13 +9,19 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
+    
+    @IBOutlet weak var nectBtn: BorderButton!
+    
     @IBAction func onNextTab(_ sender: Any) { //program to next tab
         performSegue(withIdentifier: "skillVCSegue", sender: self)
-        
-    }
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player() // initialize a struct
 
         // Do any additional setup after loading the view.
     }
@@ -25,7 +31,29 @@ class LeagueVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+        //player.desiredLeague = "mens"
+        //nectBtn.isEnabled = true    <<<------- dont repeat like this, never
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+        //player.desiredLeague = "womens"
+        //nectBtn.isEnabled = true
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+        //player.desiredLeague = "coed"
+        //nectBtn.isEnabled = true
+    }
+    
+    func selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nectBtn.isEnabled = true
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Mahmudul Hassan Tanmoy on 13/12/17.
+//  Copyright © 2017 MHT dev. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
